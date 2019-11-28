6e5324706f80e5bc7e897880b4b7221fd7716b60
eignnx <eignnx@gmail.com>
2019-11-28 03:04:54 -0500

    working!

---
+++ examples/static-file-server.rs
@@ -0,0 +1,9 @@
+use async_std::task;
+use tide_static_files::{serve_static_files, StaticDirServer};
+
+fn main() {
+    let mut app = tide::with_state(StaticDirServer::new("./examples/"));
+    app.at("/*")
+        .get(|req| async { serve_static_files(req).await.unwrap() });
+    task::block_on(app.listen("127.0.0.1:8000")).unwrap();
+}
