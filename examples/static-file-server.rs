db61f4cf5b93e7eee11d91b52c3a6d017192361a
eignnx <eignnx@gmail.com>
2019-11-29 19:22:32 -0500

    use async_std::main

--- examples/static-file-server.rs
+++ examples/static-file-server.rs
@@ -1,9 +1,9 @@
-use async_std::task;
 use tide_naive_static_files::{serve_static_files, StaticDirServer};
 
-fn main() {
+#[async_std::main]
+async fn main() {
     let mut app = tide::with_state(StaticDirServer::new("./examples/").unwrap());
     app.at("/*")
         .get(|req| async { serve_static_files(req).await.unwrap() });
-    task::block_on(app.listen("127.0.0.1:8000")).unwrap();
+    app.listen("127.0.0.1:8000").await.unwrap();
 }
