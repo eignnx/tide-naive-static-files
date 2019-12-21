6e49080bf7885a0c8273757b70e6f6ae7b3a420e
Felipe Sere <felipesere@gmail.com>
2019-12-21 19:41:17 +0100

    Update the example in the readme

--- examples/static-file-server.rs
+++ examples/static-file-server.rs
@@ -1,5 +1,6 @@
 use std::path::{Path, PathBuf};
 use tide_naive_static_files::{serve_static_files, StaticRootDir};
+use async_std::task;
 
 struct AppState {
     static_root_dir: PathBuf,
@@ -20,7 +21,7 @@ fn main() {
     app.at("static/*path")
         .get(|req| async { serve_static_files(req).await.unwrap() });
 
-    async_std::task::block_on(async move {
-        app.listen("127.0.0.1:8000").await
+    task::block_on(async move {
+        app.listen("127.0.0.1:8000").await.unwrap()
     });
 }
